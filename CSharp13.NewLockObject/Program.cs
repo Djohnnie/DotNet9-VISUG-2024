5540c7fea9b81040a60948d1ed9445069dcf4d27
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2024-10-23 09:36:51 +0200

    Initial commit with sample code

---
+++ CSharp13.NewLockObject/Program.cs
@@ -0,0 +1,47 @@
+﻿
+// Monitor-based lock object
+var lockObject = new Object();
+
+lock (lockObject)
+{
+    // ...
+}
+
+// Monitor-based lock object using Monitor
+Monitor.Enter(lockObject);
+Monitor.Exit(lockObject);
+
+
+// New System.Threading.Lock object
+var l = new Lock();
+
+// Used like new Lock EnterScope.
+using (l.EnterScope())
+{
+    // ...
+}
+
+// Used as lock object (and translated to new Lock by compiler)
+lock (l)
+{
+    // ...
+}
+
+lock ((object)l)
+{
+    // ...
+}
+
+
+//await MethodAsync();
+
+
+//static async Task MethodAsync()
+//{
+//    var l = new Lock();
+
+//    using (l.EnterScope())
+//    {
+//        await Task.Delay(1000);
+//    }
+//}
---
+++ DotNet9.JsonAdditions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using System.Text.Json.Schema;
+
+
+var options = new JsonSerializerOptions
+{
+    WriteIndented = true,
+    IndentCharacter = '\t',
+    IndentSize = 2,
+};
+
+var json1 = JsonSerializer.Serialize(new { Value = 1 }, options);
+var json2 = JsonSerializer.Serialize(new { Value = 1 }, JsonSerializerOptions.Web);
+
+Console.WriteLine(json1);
+Console.WriteLine();
+Console.WriteLine(json2);
+
+Console.ReadKey();
+
+Console.WriteLine();
+Console.WriteLine();
+
+var schema = JsonSchemaExporter.GetJsonSchemaAsNode(JsonSerializerOptions.Default, typeof(Person));
+Console.WriteLine(schema);
+
+Console.ReadKey();
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string[] Hobbies { get; set; }
+    public Address Address { get; set; }
+}
+
+public class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Zip { get; set; }
+}
